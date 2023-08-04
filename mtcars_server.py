d9a1ca3a289dad7a4f14464a42b383cdf2a674f3
Hayley Massey <Hmassey12@outlook.com>
2023-08-03 20:09:13 -0500

    update 2

--- mtcars_server.py
+++ mtcars_server.py
@@ -57,7 +57,7 @@ def get_mtcars_server_functions(input, output, session):
     # First, declare shared reactive values (used between functions) up front
     # Initialize the values on startup
 
-    reactive_location = reactive.Value("ELY MN")
+    reactive_location = reactive.Value("Kansas City US")
     reactive_stock = reactive.Value("Tesla Inc")
 
     # Previously, we had a single reactive dataframe to hold filtered results
